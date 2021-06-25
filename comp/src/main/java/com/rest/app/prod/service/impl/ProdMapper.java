27a32b65e8cb6fa06475a333f93a6d780ab7cfd3
admin <admin@YD01-13>
2021-06-25 12:06:29 +0900

    main 연결

--- comp/src/main/java/com/rest/app/prod/service/impl/ProdMapper.java
+++ comp/src/main/java/com/rest/app/prod/service/impl/ProdMapper.java
@@ -1,5 +1,9 @@
 package com.rest.app.prod.service.impl;
 
-public interface ProdMapper {
+import java.util.List;
 
+import com.rest.app.bus.vo.OrdersVO;
+
+public interface ProdMapper {
+	List<OrdersVO> getUnplannedOrders();
 }
