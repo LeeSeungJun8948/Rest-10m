8fe1cdbc98452e59c2d0131c28296d537fa04c9f
admin <admin@YD01-22>
2021-07-02 16:30:07 +0900

    소연

--- comp/src/main/java/com/rest/app/bus/service/impl/BusinessServiceImpl.java
+++ comp/src/main/java/com/rest/app/bus/service/impl/BusinessServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rest.app.bus.service.BusinessService;
+import com.rest.app.bus.vo.CompanyVO;
 import com.rest.app.bus.vo.OrdersVO;
+import com.rest.app.comm.vo.BomVO;
+import com.rest.app.mat.vo.MaterialVO;
 
 @Service("businessService")
 public class BusinessServiceImpl implements BusinessService {
@@ -22,9 +25,19 @@ public class BusinessServiceImpl implements BusinessService {
 	}
 
 	@Override
-	public List<OrdersVO> getUnExport(Map<String, Object> param) {
-		// TODO Auto-generated method stub
-		return mapper.getUnExport(param);
-		
+	public OrdersVO getUnExport(OrdersVO vo) {
+		return mapper.getUnExport(vo);
 	}
+
+	@Override
+	public List<CompanyVO> getCompany(CompanyVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.getCompany(vo);
+	}
+	@Override
+	public List<BomVO> getProduct(BomVO vo) {
+		// TODO 모달창 제품명,제품코드,규격 리스트
+		return mapper.getProduct(vo);
+	}   
+	
 }
